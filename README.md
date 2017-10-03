59fa91bedeadc20a2291e084cd4f784efa710acd
Anton Whalley <anton@venshare.com>
2017-10-03 16:48:57 +0100

    slides and README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Lightning Talk on llnode 
+
+To follow the demo in the presentation 
+
+```
+$ git clone https://github.com/no9/dublinjs-2017-10-03.git
+$ cd dublinjs-2017-10-03
+$ npm install llnode -g
+$ node --abort-on-uncaught-exception crasher.js
+$ llnode node -c NAME_OF_CORE
+(llnode) v8 bt
+(llnode) frame select 5
+(llnode) jssource
+```
--- crasher.js
+++ crasher.js
@@ -5,7 +5,7 @@ var port = 1338
 server = http.createServer(function myRequestListener(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-  res.not_a_function()
+  res.not_a_function();
 }).listen(port, host, function() {
   http.get(`http://${host}:${port}/`, (res) => {
 	console.log('This won\'t print');	
